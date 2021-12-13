3841cf50a30125f72f6fc104898d82129920eae0
Engincan VESKE <enginveske@gmail.com>
2021-12-13 18:12:00 +0300

    Use local package files instead of CDN for Syncfusion

--- src/SyncfusionComponentsDemo.Web/abp.resourcemapping.js
+++ src/SyncfusionComponentsDemo.Web/abp.resourcemapping.js
@@ -3,6 +3,7 @@ module.exports = {
         
     },
     mappings: {
-        
+        "@node_modules/@syncfusion/ej2/dist/ej2.min.js": "@libs/syncfusion/",
+        "@node_modules/@syncfusion/ej2/material.css": "@libs/syncfusion/"
     }
 };
